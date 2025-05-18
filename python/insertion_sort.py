c3791493d187cdf0aa39cdbcb79a5cd3c6f62fd1
Peterson <kinyanjuipeterson12787@gmail.com>
2025-05-18 09:40:32 +0300

    understood insertion sort; it's ez :)

--- python/insertion_sort.py
+++ python/insertion_sort.py
@@ -1,15 +1,15 @@
 list = [1,3,4,5,2,6]
 # [1,3,4,5,5,6]
 # i=0
-for i in range(1,len(list)):
-    now = list[i]
-    j=i-1
-    while j>=0 and  now < list[j]:
-        list[j + 1] = list[j]
-        j-=1
-    list[j + 1] = now
+# for i in range(1,len(list)):
+#     now = list[i]
+#     j=i-1
+#     while j>=0 and  now < list[j]:
+#         list[j + 1] = list[j]
+#         j-=1
+#     list[j + 1] = now
 
-print(list)
+# print(list)
 
 # tmp = 0
 # i=0
@@ -27,6 +27,25 @@ print(list)
 #     i+=1
 
 # print(list)
+
+
+#ok am back understanding what to do
+#list = [1,3,4,5,2,6]
+
+print(len(list))
+
+for i in range(1, len(list)):
+    key = list[i]
+    j = i-1
+    while list[j]>key and j > 0:
+        print(f"{j} is greater than {key}")
+        list[j +1] = list[j]
+        j = j-1
+    list[j +1] = key
+print(list)
+
+#exceptionally ez :)
+
 
 
     
